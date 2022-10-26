b0cfec3ad208f626d4a408769639a126c9ae6a76
rjpeterson <r.james.peterson@gmail.com>
2022-10-26 18:12:29 +0800

    new exercises

---
+++ APILearnSketch/APILearnSketch.py
@@ -0,0 +1,24 @@
+#Author-
+#Description-
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        design = adsk.fusion.Design.cast(app.activeProduct)
+        rootComp = design.rootComponent
+        sketches = rootComp.sketches
+        
+        sketch: adsk.fusion.Sketch = sketches.add(rootComp.xYConstructionPlane)
+        sketchLines = sketch.sketchCurves.sketchLines
+        startPoint = adsk.core.Point3D.create(0,0,0)
+        endPoint = adsk.core.Point3D.create(5,5,0)
+        sketchLines.addCenterPointRectangle(startPoint, endPoint)
+        sketchLines.addByTwoPoints(startPoint, endPoint)
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
