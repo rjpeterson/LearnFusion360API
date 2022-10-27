5e1debfeef3a261ed18770033d3dfabff3213eae
rjpeterson <r.james.peterson@gmail.com>
2022-10-27 08:05:20 +0800

    cast from activeEditObject

--- APILearnSketch/APILearnSketch.py
+++ APILearnSketch/APILearnSketch.py
@@ -8,11 +8,12 @@ def run(context):
     try:
         app = adsk.core.Application.get()
         ui  = app.userInterface
-        design = adsk.fusion.Design.cast(app.activeProduct)
-        rootComp = design.rootComponent
-        sketches = rootComp.sketches
+        # design = adsk.fusion.Design.cast(app.activeProduct)
+        # rootComp = design.rootComponent
+        # sketches = rootComp.sketches
         
-        sketch: adsk.fusion.Sketch = sketches.add(rootComp.xYConstructionPlane)
+        # sketch: adsk.fusion.Sketch = sketches.add(rootComp.xYConstructionPlane)
+        sketch = adsk.fusion.Sketch.cast(app.activeEditObject)
         sketchLines = sketch.sketchCurves.sketchLines
         startPoint = adsk.core.Point3D.create(0,0,0)
         endPoint = adsk.core.Point3D.create(5,5,0)
