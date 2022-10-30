57e8d54750f5458e2d61cc199e95cbef9d939d1c
rjpeterson <r.james.peterson@gmail.com>
2022-10-30 15:30:54 +0800

    add command input box and preview

--- BodyFromHash/BodyFromHash.py
+++ BodyFromHash/BodyFromHash.py
@@ -3,6 +3,10 @@
 
 import adsk.core as core, adsk.fusion as fusion, adsk.cam, traceback
 
+defaultHash = '0000000000000000000700ee25d025cf3a2e29706b74b55c0a39d46206c569a2'
+defaultBase = '16'
+
+
 directions = {
     'XPOS': '0',
     'XNEG': '1',
@@ -21,45 +25,98 @@ inverseDirections = {
     'ZNEG': 'ZPOS'
 }
 
-# Convert integer to baseX where x <= 36
-def base(decimal: int, base: int) :
-    if base < 2 or base > 36:
-        raise Exception('Supplied base must be between 2 and 36')
-    list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    answer = ""
-    while decimal != 0 :
-        answer  += list[decimal % base]
-        decimal //= base
-    return answer[::-1]
+handlers = []
+createPoint = core.Point3D.create
+app = core.Application.get()
+if app:
+    ui  = app.userInterface
+    alert = ui.messageBox
 
-def directionFromDigit(digit: str):
-    for key, value in directions.items():
-        if digit == value:
-            return key
+class BodyFromHashExecuteHandler(core.CommandEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args: core.CommandEventArgs):
+        try:
+            unitsMgr = app.activeProduct.unitsManager
+            command: core.Command = args.firingEvent.sender
+            inputs = command.commandInputs
 
-def run(context):
-    ui = None
-    try:
-        app = core.Application.get()
-        ui  = app.userInterface
+            bodyFromHash = BodyFromHash()
+            for input in inputs:
+                if input.id == 'hash':
+                    bodyFromHash.hash = input.value
+                elif input.id == 'base':
+                    bodyFromHash.base = input.value
 
-        alert = ui.messageBox
-        design: fusion.Design = app.activeProduct
-        createPoint = core.Point3D.create
-        if not design:
-            ui.messageBox('It is not supported in current workspace, please change to MODEL workspace and try again.')
-            return
+            bodyFromHash.createBody()
+            args.isValidResult = True
+        except:
+            if ui:
+                alert('Failed:\n{}'.format(traceback.format_exc()))
 
-        retVals = ui.inputBox('Please input a blockHash', 'BodyFromHash', '0000000000000000000700ee25d025cf3a2e29706b74b55c0a39d46206c569a2')
-        if retVals[0]:
-                (hash, isCancelled) = retVals
-            
-        # Exit the program if the dialog was cancelled.
-        if isCancelled:
-            return
-        base10Hash = int(hash, base=16)
+class BodyFromHashDestroyHandler(core.CommandEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        try:
+            # when the command is done, terminate the script
+            # this will release all globals which will remove all event handlers
+            adsk.terminate()
+        except:
+            if ui:
+                ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+class BodyFromHashCommandCreatedHandler(core.CommandCreatedEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args: core.CommandCreatedEventArgs):
+        try:
+            cmd = args.command
+            cmd.isRepeatable = False
+            onExecute = BodyFromHashExecuteHandler()
+            cmd.execute.add(onExecute)
+            onExecutePreview = BodyFromHashExecuteHandler()
+            cmd.executePreview.add(onExecutePreview)
+            onDestroy = BodyFromHashDestroyHandler()
+            cmd.destroy.add(onDestroy)
+            # keep the handlers referenced beyond this function
+            handlers.append(onExecute)
+            handlers.append(onExecutePreview)
+            handlers.append(onDestroy)
+
+            inputs = cmd.commandInputs
+            inputs.addStringValueInput('hash', 'Hash', defaultHash)
+            inputs.addStringValueInput('base', 'Base', defaultBase)
+
+        except:
+            if ui:
+                alert('Failed:\n{}'.format(traceback.format_exc()))
+
+class BodyFromHash:
+    def __init__(self):
+        self._hash = defaultHash
+        self._base = defaultBase
+
+    #properties
+    @property
+    def hash(self):
+        return self._hash
+    @hash.setter
+    def hash(self, value):
+        self._hash = value
+    
+    @property
+    def base(self):
+        return self._base
+    @base.setter
+    def base(self, value):
+        self._base = value
+
+    def createBody(self):
+        base10Hash = int(self._hash, int(self._base))
         base6Hash = base(base10Hash, 6)
 
+        design: fusion.Design = app.activeProduct
         root = design.rootComponent
         sketches = root.sketches
 
@@ -104,10 +161,51 @@ def run(context):
         createPipe(root, curvesCollection, 0.2, 0.04)
         app.activeViewport.refresh()
 
+    
+
+def run(context):
+    try:
+        design: fusion.Design = app.activeProduct
+        if not design:
+            ui.messageBox('It is not supported in current workspace, please change to MODEL workspace and try again.')
+            return
+        commandDefinitions = ui.commandDefinitions
+        #check the command exists or not
+        cmdDef = commandDefinitions.itemById('BodyFromHash')
+        if not cmdDef:
+            cmdDef = commandDefinitions.addButtonDefinition('BodyFromHash',
+                    'Create Body from a Hash',
+                    'Create a body from a hash.',
+                    './resources') # relative resource file path is specified
+
+        onCommandCreated = BodyFromHashCommandCreatedHandler()
+        cmdDef.commandCreated.add(onCommandCreated)
+        # keep the handler referenced beyond this function
+        handlers.append(onCommandCreated)
+        inputs = adsk.core.NamedValues.create()
+        cmdDef.execute(inputs)
+
+        # prevent this module from being terminate when the script returns, because we are waiting for event handlers to fire
+        adsk.autoTerminate(False)
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
+# Convert integer to baseX where x <= 36
+def base(decimal: int, base: int) :
+    if base < 2 or base > 36:
+        raise Exception('Supplied base must be between 2 and 36')
+    list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    answer = ""
+    while decimal != 0 :
+        answer  += list[decimal % base]
+        decimal //= base
+    return answer[::-1]
+
+def directionFromDigit(digit: str):
+    for key, value in directions.items():
+        if digit == value:
+            return key
 
 def createPipe(comp: fusion.Component, curves: fusion.SketchCurves, radius: int, pipeThickness: int):
     # create path
@@ -157,4 +255,3 @@ def createPipe(comp: fusion.Component, curves: fusion.SketchCurves, radius: int,
     shellInput = shellFeats.createInput(objCol, False)
     shellInput.insideThickness = core.ValueInput.createByReal(pipeThickness)
     shellFeats.add(shellInput)
-    
