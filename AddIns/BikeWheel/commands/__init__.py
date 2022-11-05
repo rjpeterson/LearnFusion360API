96581637e7abec397e7047422de7f342a9271ea6
rjpeterson <r.james.peterson@gmail.com>
2022-11-05 15:31:41 +0800

    rim addin boilderplate

--- AddIns/BikeWheel/commands/__init__.py
+++ AddIns/BikeWheel/commands/__init__.py
@@ -3,18 +3,14 @@
 # Import the modules corresponding to the commands you created.
 # If you want to add an additional command, duplicate one of the existing directories and import it here.
 # You need to use aliases (import "entry" as "my_module") assuming you have the default module named "entry".
-from .commandDialog import entry as commandDialog
-from .paletteShow import entry as paletteShow
-from .paletteSend import entry as paletteSend
 from .spoke import entry as spoke
+from .rim import entry as rim
 
 # add your imported modules to this list.
 # Fusion will automatically call the start() and stop() functions.
 commands = [
-    commandDialog,
-    paletteShow,
-    paletteSend,
-    spoke
+    spoke,
+    rim
 ]
 
 
