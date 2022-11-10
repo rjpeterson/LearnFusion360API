32520b4a8aa98d17a7fffdbc1139db9e88d470a9
rjpeterson <r.james.peterson@gmail.com>
2022-11-10 14:39:08 +0800

    adjust config

--- AddIns/BikeWheel/config.py
+++ AddIns/BikeWheel/config.py
@@ -17,5 +17,10 @@ DEBUG = True
 ADDIN_NAME = os.path.basename(os.path.dirname(__file__))
 COMPANY_NAME = 'RJP'
 
-# Palettes
-sample_palette_id = f'{COMPANY_NAME}_{ADDIN_NAME}_palette_id'
+design_workspace = 'FusionSolidEnvironment'
+tools_tab_id = "ToolsTab"
+my_tab_name = "Bike Wheel"  # Only used if creating a custom Tab
+
+my_panel_id = f'{ADDIN_NAME}_panel_2'
+my_panel_name = ADDIN_NAME
+my_panel_after = ''
